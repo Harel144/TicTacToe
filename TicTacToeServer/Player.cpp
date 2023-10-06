4d29e58ecfa9c271ecc2070daaaf0c227bf176b4
N0tPVan5 <arel2226a@gmail.com>
2023-10-06 05:11:48 +0300

    Wrote Player class implementation at Player.cpp

--- TicTacToeServer/Player.cpp
+++ TicTacToeServer/Player.cpp
@@ -1,1 +1,26 @@
 #pragma once
+#include "Player.h"
+
+Player::Player(char type, string name) : name(name), type(type)
+{
+}
+
+char Player::getType()
+{
+	return this->type;
+}
+
+string Player::getName()
+{
+	return this->name;
+}
+
+void Player::setType(char type)
+{
+	this->type = type;
+}
+
+void Player::setName(string name)
+{
+	this->name = name;
+}
