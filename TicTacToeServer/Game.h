89918fdc2ad01004a73099d8a417c6500ffce82d
N0tPVan5 <arel2226a@gmail.com>
2023-10-06 05:22:54 +0300

    Wrote Game class at Game.h

--- TicTacToeServer/Game.h
+++ TicTacToeServer/Game.h
@@ -1,1 +1,23 @@
 #pragma once
+#include "Player.h"
+
+#define ROWS 3
+#define COLS 3
+
+class Game
+{
+private:
+	Player xPlayer;
+	Player oPlayer;
+	bool isXTurn;
+	char gameBoard[ROWS][COLS];
+	
+	bool changeTurn();
+
+public:
+	Game();
+	bool checkWin();
+	void playTurn();
+	char* getBoard();
+	bool getTurn();
+};
--- TicTacToeServer/Player.h
+++ TicTacToeServer/Player.h
@@ -8,13 +8,13 @@ const char o = 'O';
 
 class Player 
 {
+private:
+	char type;
+	string name;
 public:
 	Player(char type, string name);
 	char getType();
 	string getName();
 	void setType(char type);
 	void setName(string name);
-private:
-	char type;
-	string name;
 };
