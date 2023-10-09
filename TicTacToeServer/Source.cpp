a93e9d3af78e1ed20fb8350c3944421f946e585b
N0tPVan5 <arel2226a@gmail.com>
2023-10-09 05:47:25 +0300

    Added name input

--- TicTacToeServer/Source.cpp
+++ TicTacToeServer/Source.cpp
@@ -3,5 +3,16 @@
 
 int main(void)
 {
+	std::cout << "enter first player's name: ";
+	std::string firstName;
+	std::cin >> firstName;
+
+	std::cout << "enter second player's name: ";
+	std::string secondName;
+	std::cin >> secondName;
+
+	Game newGame(firstName, secondName);
+	
+
 	return 0;
 }
