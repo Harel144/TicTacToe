c7948f31d3efe17387298ad7dd9c5ff3d5cfd899
N0tPVan5 <arel2226a@gmail.com>
2023-10-11 00:42:10 +0300

    Wrote JoinLobby and CreateLobby structs at RequestStructs.h

--- TicTacToeServer/RequestStructs.h
+++ TicTacToeServer/RequestStructs.h
@@ -1,2 +1,21 @@
 #pragma once
+#include <string>
 
+struct CreateLobby
+{
+	std::string lobbyName;
+	int gridSize;
+};
+
+struct JoinLobby
+{
+	std::string lobbyName;
+};
+
+struct SendTurnData
+{
+	bool xWon;
+	bool oWon;
+	char** gameBoard;
+	bool isXTurn;
+};
