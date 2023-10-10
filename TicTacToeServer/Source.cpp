4496f48afe22824a0683826c1d32c6bef19ed476
N0tPVan5 <arel2226a@gmail.com>
2023-10-10 05:31:51 +0300

    Changed main to a server

--- TicTacToeServer/Source.cpp
+++ TicTacToeServer/Source.cpp
@@ -1,69 +1,23 @@
-#include "Game.h"
+#pragma comment (lib, "ws2_32.lib")
 
-int main(void)
+#include "WSAInitializer.h"
+#include "Server.h"
+#include <iostream>
+#include <exception>
+
+int main()
 {
-	std::cout << "enter first player's name: ";
-	std::string firstName;
-	std::cin >> firstName;
-
-	std::cout << "enter second player's name: ";
-	std::string secondName;
-	std::cin >> secondName;
-
-	Game* newGame = new Game(firstName, secondName);
-
-	std::cout << "============\n"
-		"  WELCOME\n"
-		"============\n" << std::endl;
-
-	std::cout << "current board:\n" << std::endl;
-	newGame->printBoard();
-
-	while (!newGame->checkWin(x) && !newGame->checkWin(o))
+	try
 	{
-		int loc = 0;
-		if (newGame->getTurn())
-		{
-			std::cout << "send location (0-8) to place an X: ";
-			std::cin >> loc;
+		WSAInitializer wsaInit;
+		Server myServer;
 
-			bool changed = newGame->playTurn(loc);
-
-			while (!changed)
-			{
-				std::cout << "Invalid! send location (0-8) to place an X: ";
-				std::cin >> loc;
-
-				changed = newGame->playTurn(loc);
-			}
-		}
-		else
-		{
-			std::cout << "send location (0-8) to place an O: ";
-			std::cin >> loc;
-
-			bool changed = newGame->playTurn(loc);
-
-			while (!changed)
-			{
-				std::cout << "Invalid! send location (0-8) to place an O: ";
-				std::cin >> loc;
-
-				changed = newGame->playTurn(loc);
-			}
-		}
-
-		newGame->printBoard();
+		myServer.serve(8876);
 	}
-
-	if (newGame->checkWin(x))
+	catch (std::exception& e)
 	{
-		std::cout << "\nCONGRATS X!";
+		std::cout << "Error occured: " << e.what() << std::endl;
 	}
-	else
-	{
-		std::cout << "\nCONGRATS O!";
-	}
-
+	system("PAUSE");
 	return 0;
 }
