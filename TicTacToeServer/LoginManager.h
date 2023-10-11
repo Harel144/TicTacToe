dd474d4a56649ff21efd24769a215e0611c2734f
N0tPVan5 <arel2226a@gmail.com>
2023-10-12 02:12:37 +0300

    Added LoginManager.cpp. Turned LoginManager into a Singleton class

--- TicTacToeServer/LoginManager.h
+++ TicTacToeServer/LoginManager.h
@@ -5,9 +5,18 @@
 class LoginManager
 {
 public:
-	bool addPlayer(Player newPlayer);
-	void removePlayer(std::string playerName);
-	bool isPlayerConnected(std::string playerName);
+    // Static method to access the singleton instance
+    static LoginManager& getInstance()
+    {
+        static LoginManager instance; // This ensures a single instance is created
+        return instance;
+    }
+
+    bool addPlayer(Player newPlayer);
+    void removePlayer(std::string playerName);
+    bool isPlayerConnected(std::string playerName);
+
 private:
-	std::vector<Player> players;
+    LoginManager(); // Private constructor to prevent external instantiation
+    std::vector<Player> players;
 };
